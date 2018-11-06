776a777f8f9c65ef1c3e46d97052589958c6ddb1
Luis <luis_lopez@dlsu.edu.ph>
2018-11-06 21:45:59 +0800

    Moved routes from home.js into their respective controllers

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,7 +1,7 @@
 module.exports.controller = function (app) {
 
-  app.get('/contact-us', (req, res)=>{
-    res.send('The "contact us" page.')
+  app.get('/contact', (req, res)=>{
+    res.render('contact.ejs')
   })
 
 }
--- controllers/home.js
+++ controllers/home.js
@@ -3,10 +3,5 @@ module.exports.controller = function (app) {
   app.get('/', (req, res)=>{
     res.render('home.ejs')
   })
-  app.get("/contact", (req, res)=>{
-    res.render('contact.ejs')
-})
-app.get("/services", (req, res)=>{
-  res.render('services.ejs')
-})
+
 }
