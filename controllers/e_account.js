b2fb80651accbf8f1dbf80f70b87af1156c3e2bb
luigirivera <luigirivera@users.noreply.github.com>
2018-11-10 20:45:27 +0800

    created employee pages

---
+++ controllers/e_account.js
@@ -0,0 +1,9 @@
+module.exports.controller = function (app) {
+
+  app.get('/employee/account', (req, res)=>{
+    res.render('admin/employee/account.ejs')
+  })
+
+}
+
+module.exports.controller = function (app) {}
---
+++ controllers/e_orders.js
@@ -0,0 +1,9 @@
+module.exports.controller = function (app) {
+
+  app.get('/employee/orders', (req, res)=>{
+    res.render('admin/employee/orders.ejs')
+  })
+
+}
+
+module.exports.controller = function (app) {}
