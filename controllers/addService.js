9f0a218ded7d3e43af84f049b200dfa60c906439
luigirivera <luigirivera@users.noreply.github.com>
2018-11-10 14:48:48 +0800

    fixed indentation and added AddService ejs

---
+++ controllers/addService.js
@@ -0,0 +1,9 @@
+module.exports.controller = function (app) {
+
+  app.get('/admin/services/add_service', (req, res)=>{
+    res.render('admin/addService.ejs')
+  })
+
+}
+
+module.exports.controller = function (app) {}
