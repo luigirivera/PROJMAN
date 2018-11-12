21b2e113a6424de7682d5de90d1b44849d65bb4c
Luis <luis_lopez@dlsu.edu.ph>
2018-11-12 23:45:52 +0800

    userService functions return doc without password

--- models/userService.js
+++ models/userService.js
@@ -5,7 +5,7 @@ const User = require(path.join(__dirname, 'User.js')).User
 function getAll() {
   return new Promise(
     function(resolve, reject) {
-      User.find({}, 'username').sort({ username : 1 }).populate('type')
+      User.find({}, '-password').sort({ username : 1 }).populate('type')
       .then((users)=>{
         if (users) resolve(users)
         else throw Error('No users')
@@ -20,7 +20,7 @@ function getAll() {
 function getByUsername(username) {
   return new Promise(
     function(resolve, reject) {
-      User.findOne({username}, 'username')
+      User.findOne({username}, '-password')
       .populate('type')
       .then((user)=>{
         if (user) resolve(user)
