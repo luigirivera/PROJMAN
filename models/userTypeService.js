3aec5b2ccbf2a301d003a1380c5cf163364d29d4
Luis <luis_lopez@dlsu.edu.ph>
2018-11-12 20:28:29 +0800

    Added userTypeService.js

---
+++ models/userTypeService.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+const UserType = require(path.join(__dirname, 'UserType.js')).UserType
+
+function getAll() {
+  return new Promise(
+    function(resolve, reject) {
+      UserType.find({})
+      .then((userTypes)=>{
+        if (userTypes) resolve(userTypes)
+        else throw Error('No user types')
+      })
+      .catch((err)=>{
+        reject(err)
+      })
+    }
+  )
+}
+
+function getById(id) {
+  return new Promise(
+    function(resolve, reject) {
+      UserType.findOne({ _id : id })
+      .then((userType)=>{
+        if (userType) resolve(userType)
+        else throw Error('User type with id ' + id + ' not found')
+      })
+      .catch((err)=>{
+        reject(err)
+      })
+    }
+  )
+}
+
+function getBySystemName(name) {
+  return new Promise(
+    function(resolve, reject) {
+      UserType.findOne({ system_name : name })
+      .then((userType)=>{
+        if (userType) resolve(userType)
+        else throw Error('User type with system name ' + name + ' not found')
+      })
+      .catch((err)=>{
+        reject(err)
+      })
+    }
+  )
+}
+
+module.exports = {getAll, getById, getBySystemName}
